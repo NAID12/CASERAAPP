1059737502d8ffa7f4369fcde393bb7db7700d2a
Nasreldin Aid <nasreldinaid@outlook.com>
2017-01-27 21:07:23 -0800

    Added Login Button and Cancel Button

--- app/src/main/java/naid12/caseraapp/MainActivity.java
+++ app/src/main/java/naid12/caseraapp/MainActivity.java
@@ -2,12 +2,35 @@ package naid12.caseraapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+  EditText LoginName;
+  CheckBox chBoxStayLogged;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      LoginName = (EditText) findViewById(R.id.LoginName);
     }
+
+  public void btnLoginOnClick(View v) {
+    Log.i("Click", "Button was cliced");
+    Log.i("Click", LoginName.getText().toString());
+    Toast.makeText(this, "Welcome to Caasera, you have succesfuly Logged in." +
+        LoginName.getText().toString(), Toast.LENGTH_LONG).show(); // + " " +
+    // chBoxStayLoggedin.getText().toString(), Toast.LENGTH_LONG).show();
+  }
+    /*public void btnLoginOnClick(  View v) {
+        Log.i("Click", "Button wes clicked!");
+
+        Toast.makeText(this, "Welcome to Caasera, We have succesfuly Logged in." +
+            LoginName.getText().toString() + " " +
+            chBoxStayLoggedin.getText().toString(), Toast.LENGTH_LONG).show();
+    }*/
 }
